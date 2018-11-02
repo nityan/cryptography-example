344a8d2c0a490e14bf13e5e0623d45c7772d8644
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2018-11-02 09:52:55 -0400

    added initial Hashing code

--- CryptographyExample/Controllers/HomeController.cs
+++ CryptographyExample/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CryptographyExample.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace CryptographyExample.Controllers
 {
@@ -12,6 +14,19 @@ namespace CryptographyExample.Controllers
 	{
 		public IActionResult Index()
 		{
+			// generate a 128-bit salt using a secure PRNG
+			var salt = new byte[128 / 8];
+
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(salt);
+			}
+
+			Debug.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+			// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+			var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2("some text password here", salt, KeyDerivationPrf.HMACSHA1, 10000, 256 / 8));
+
 			return View();
 		}
 

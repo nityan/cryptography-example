c8fa6164b09a855b934511956ddb3668a69cd6a5
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2018-11-04 18:26:53 -0500

    added documentation

--- CryptographyExample/Controllers/HomeController.cs
+++ CryptographyExample/Controllers/HomeController.cs
@@ -17,15 +17,22 @@ namespace CryptographyExample.Controllers
 			// generate a 128-bit salt using a secure PRNG
 			var salt = new byte[128 / 8];
 
+			// generate the salt value
+			// generate a salt value which we will use to add "randomness" to the hash
 			using (var rng = RandomNumberGenerator.Create())
 			{
 				rng.GetBytes(salt);
 			}
 
+			// write the salt value to the output
 			Debug.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
 			// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+			// iterate 10,000 times to create a hash of the plain text password and the salt value
+			// using the PBKDF2 algorithm
 			var hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2("some text password here", salt, KeyDerivationPrf.HMACSHA1, 10000, 256 / 8));
+
+			Debug.WriteLine(hashed);
 
 			return View();
 		}
